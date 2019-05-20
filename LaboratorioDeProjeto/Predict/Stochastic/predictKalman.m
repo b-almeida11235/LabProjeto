6327ea351666b5c99f0ff6f4a93042a43302e44f
b-almeida11235 <32137976+b-almeida11235@users.noreply.github.com>
2019-05-20 12:10:36 +0100

    Update predictKalman.m

--- LaboratorioDeProjeto/Predict/Stochastic/predictKalman.m
+++ LaboratorioDeProjeto/Predict/Stochastic/predictKalman.m
@@ -1,13 +1,13 @@
 %TODO: verificar tamanho das matrizes
-%      Há parâmetros que têm de ser passados num vetor
+%      HÃ¡ parÃ¢metros que tÃªm de ser passados num vetor
  
-function [x, p] = predictKalman(struct)
-    f = struct.f;
-    x = struct.x;
-    b = struct.b;
-    u = struct.u;
-    q = struct.q;
-    p = struct.p;
+function [x, p] = predictKalman(values)
+    f = values.f;
+    x = values.x;
+    b = values.b;
+    u = values.u;
+    q = values.q;
+    p = values.p;
     ft = f';
     
     %if (size(f, 2) ~= size(x, 1) || size(f, 2) ~= size(p, 1) || size(b, 2) ~= size(u, 1) || size(p, 2) ~= size(ft, 1)):
