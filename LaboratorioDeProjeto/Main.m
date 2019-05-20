aca9917199f18b1121d5cab7b5cdc2f530075e1d
b-almeida11235 <32137976+b-almeida11235@users.noreply.github.com>
2019-05-20 11:58:05 +0100

    Update Main.m

--- LaboratorioDeProjeto/Main.m
+++ LaboratorioDeProjeto/Main.m
@@ -11,6 +11,24 @@ struct.h = [1, 0, 0, 0; 0, 1, 0, 0];
 struct.r = [0.0225, 0; 0, 0.0225];
 struct.z = zeros(2, 1);
 
+m = 1000;
+b = 50;
+v = 10;
+t = 0:0.1:10;
+u = 500*ones(size(t));
+
+s = tf('s');
+P_cruise = 1/(m*s+b);
+
+Ts = 1/50;
+
+dP_cruise = c2d(P_cruise,Ts,'zoh');
+
+display(dP_cruise);
+struct.f = -b/m;
+struct.x = v;
+struct.b = 1/m;
+struct.h = 1;
 
 %z = predictKalman(struct);
-zz = updateKalman(struct);
+%zz = updateKalman(struct);
